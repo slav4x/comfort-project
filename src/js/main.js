1f602fb447d949b85d132eb937b835994ffcbc52
Slava Khripushin <slava.cpa@yandex.ru>
2024-05-30 23:11:27 +0700

    feat: responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable no-inner-declarations */
 
-const viewportFix = (width) => {
-  const meta = document.querySelector('meta[name="viewport"]');
-  meta.setAttribute('content', `user-scalable=no, width=${screen.width <= width ? width : 'device-width'}`);
-};
+// const viewportFix = (width) => {
+//   const meta = document.querySelector('meta[name="viewport"]');
+//   meta.setAttribute('content', `user-scalable=no, width=${screen.width <= width ? width : 'device-width'}`);
+// };
 
-viewportFix(380);
+// viewportFix(475);
 
 document.addEventListener('DOMContentLoaded', function () {
   Fancybox.bind('[data-fancybox]', {
